8b37f1216f55b56732a2d45c260b37b0bea833e9
pelDev <pelumidevelops18@gmail.com>
2023-09-09 13:28:12 +0100

    fix gretting func

--- src/components/Actionbar/index.tsx
+++ src/components/Actionbar/index.tsx
@@ -17,7 +17,7 @@ export default function Actionbar(props: Props) {
         const hour = today.getHours();
         if (hour < 12) {
             return "Good morning!";
-        } else if (hour >= 12 && hour < 4) {
+        } else if (hour >= 12 && hour < 16) {
             return "Good afternoon!";
         } else {
             return "Good evening!";
